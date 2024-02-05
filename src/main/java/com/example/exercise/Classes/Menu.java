ca2176a4582ad522890376d76724283d104e3ff0
Daniele Cagnoni <145670818+Sneakydan7@users.noreply.github.com>
2024-02-05 17:34:36 +0100

    second commit

--- src/main/java/com/example/exercise/Classes/Menu.java
+++ src/main/java/com/example/exercise/Classes/Menu.java
@@ -17,5 +17,20 @@ public class Menu {
         menuItems.add(item);
     }
 
+    public static void showMenu(Menu menu) {
+        for (MenuObject item : menu.getMenuItems()) {
+            if (item instanceof Pizza pizza) {
+                System.out.println("------PIZZA------");
+                System.out.println(item.getName() + "-" + item.getPrice() + "$" + "-- calories" + pizza.getCalories());
+                System.out.println("Toppings" + pizza.getToppings());
+            } else if (item instanceof Drink) {
+                System.out.println("------DRINK------");
+                System.out.println(item.getName() + "-" + item.getPrice() + "$");
+            } else if (item instanceof Topping) {
+                System.out.println("------TOPPING------");
+                System.out.println(item.getName() + "-" + item.getPrice() + "$");
 
+            }
+        }
+    }
 }
--- src/main/java/com/example/exercise/DemoApplication.java
+++ src/main/java/com/example/exercise/DemoApplication.java
@@ -14,24 +14,9 @@ public class DemoApplication {
 
 
         Menu m = (Menu) ctx.getBean("menu");
-        printMenu(m);
+        Menu.showMenu(m);
 
     }
 
-    private static void printMenu(Menu menu) {
-        for (MenuObject item : menu.getMenuItems()) {
-            if (item instanceof Pizza pizza) {
-                System.out.println("------PIZZA------");
-                System.out.println(item.getName() + "-" + item.getPrice() + "$" + "-- calories" + pizza.getCalories());
-                System.out.println("Toppings" + pizza.getToppings());
-            } else if (item instanceof Drink) {
-                System.out.println("------DRINK------");
-                System.out.println(item.getName() + "-" + item.getPrice() + "$");
-            } else if (item instanceof Topping) {
-                System.out.println("------TOPPING------");
-                System.out.println(item.getName() + "-" + item.getPrice() + "$");
 
-            }
-        }
-    }
 }
